6e15ec9612d3072c364f4e898d95cf95952b1e51
Mitchell Simoens <ms.simoens@gmail.com>
2011-12-19 19:10:18 -0600

    Add a paging feature and example

---
+++ Ext.ux.touch.grid/feature/Paging.js
@@ -0,0 +1,191 @@
+Ext.define('Ext.ux.touch.grid.feature.Paging', {
+    extend   : 'Ext.ux.touch.grid.feature.Abstract',
+    requires : 'Ext.ux.touch.grid.feature.Abstract',
+
+    config : {
+        backButton    : {
+            text : 'Previous Page',
+            ui   : 'back'
+        },
+        forwardButton : {
+            text : 'Next Page',
+            ui   : 'forward'
+        },
+        goToButton    : {
+            text : 'Go to page...'
+        },
+
+        pager         : {
+            xtype  : 'toolbar',
+            docked : 'top',
+            layout : {
+                type : 'hbox'
+            }
+        },
+
+        pageListTpl : 'Page {page}',
+        goToPicker  : null,
+        pages       : 0
+    },
+
+    backText : 'back',
+
+    init : function(grid) {
+        var me         = this,
+            backBtn    = me.getBackButton(),
+            forwardBtn = me.getForwardButton(),
+            goToBtn    = me.getGoToButton(),
+            pager      = me.getPager();
+
+        Ext.apply(backBtn, {
+            action   : 'back',
+            disabled : true,
+            scope    : this,
+            handler  : 'handleBackButton'
+        });
+
+        Ext.apply(forwardBtn, {
+            action   : 'forward',
+            disabled : true,
+            scope    : this,
+            handler  : 'handleForwardButton'
+        });
+
+        Ext.apply(goToBtn, {
+            action   : 'goTo',
+            disabled : true,
+            scope    : this,
+            handler  : 'handleGoToButton'
+        });
+
+        Ext.apply(pager, {
+            items  : [
+                backBtn,
+                {
+                    xtype : 'spacer'
+                },
+                goToBtn,
+                {
+                    xtype : 'spacer'
+                },
+                forwardBtn
+            ]
+        });
+
+        me.setPager(
+            pager = grid.insert(0, pager)
+        );
+
+        me.setBackButton(
+            pager.down('button[action=back]')
+        );
+        me.setForwardButton(
+            pager.down('button[action=forward]')
+        );
+        me.setGoToButton(
+            pager.down('button[action=goTo]')
+        );
+
+        grid.on('painted', 'handleGridPaint', this, { buffer : 50 });
+    },
+
+    handleGridPaint : function(grid) {
+        if (!(grid instanceof Ext.ux.touch.grid.View)) {
+            grid = this.grid;
+        }
+
+        var me    = this,
+            store = grid.getStore();
+
+        if (store.isLoading()) {
+            store.on('load', 'handleGridPaint', this);
+            return;
+        }
+
+        var total         = store.getTotalCount(),
+            currentPage   = store.currentPage,
+            pages         = Math.ceil(total / store.pageSize),
+            backButton    = me.getBackButton(),
+            forwardButton = me.getForwardButton(),
+            goToButton    = me.getGoToButton();
+
+        me.setPages(pages);
+
+        backButton   .setDisabled(currentPage == 1);
+        forwardButton.setDisabled(currentPage == pages);
+        goToButton   .setDisabled(pages       == 0);
+    },
+
+    handleBackButton : function(btn) {
+        var grid  = this.grid,
+            store = grid.getStore();
+
+        store.previousPage();
+    },
+
+    handleForwardButton : function(btn) {
+        var grid  = this.grid,
+            store = grid.getStore();
+
+        store.nextPage();
+    },
+
+    handleGoToButton : function(btn) {
+        var me     = this,
+            picker = me.getGoToPicker(),
+            pages  = me.getPages(),
+            i      = 1,
+            data   = [];
+
+        if (!picker) {
+            picker = me.buildPicker();
+        }
+
+        var store = picker.down('list').getStore();
+
+        store.removeAll();
+
+        for (; i <= pages; i++) {
+            data.push({ page : i });
+        }
+
+        store.add(data);
+
+        picker.showBy(btn);
+    },
+
+    buildPicker : function() {
+        return Ext.create('Ext.Panel', {
+            centered : true,
+            modal    : true,
+            width    : 200,
+            height   : 200,
+            layout   : 'fit',
+            items    : [
+                {
+                    xtype   : 'list',
+                    itemTpl : this.getPageListTpl(),
+                    store   : Ext.create('Ext.data.Store', {
+                        fields : [
+                            'page'
+                        ]
+                    }),
+                    listeners : {
+                        scope   : this,
+                        itemtap : 'handlePageSelect'
+                    }
+                }
+            ]
+        });
+    },
+
+    handlePageSelect : function(list, index) {
+        var panel = list.up('panel'),
+            store = this.grid.getStore(),
+            page  = index + 1;
+
+        store.loadPage(page);
+
+        panel.hide();
+    }
+});
---
+++ paging.js
@@ -0,0 +1,85 @@
+Ext.Loader.setConfig({
+    enabled : true,
+    paths   : {
+        'Ext.ux.touch.grid': './Ext.ux.touch.grid'
+    }
+});
+
+Ext.require([
+    'Ext.ux.touch.grid.View',
+    'Ext.ux.touch.grid.feature.Feature',
+    'Ext.ux.touch.grid.feature.Paging',
+    'Ext.ux.touch.grid.feature.Sorter'
+]);
+
+Ext.define('TestModel', {
+    extend : 'Ext.data.Model',
+
+    fields : [
+        'firstName',
+        'lastName',
+        {
+            name    : 'fullName',
+            convert : function(val, rec) {
+                var data = rec.data;
+                return data.lastName + ', ' + data.firstName;
+            }
+        }
+    ],
+
+    proxy : {
+        type   : 'ajax',
+        url    : 'data.php',
+        reader : {
+            type : 'json',
+            root : 'data'
+        }
+    }
+});
+
+Ext.setup({
+    onReady: function() {
+        var store = Ext.create('Ext.data.Store', {
+			model    : 'TestModel',
+            autoLoad : true
+		});
+
+        Ext.create('Ext.ux.touch.grid.View', {
+            fullscreen : true,
+            store      : store,
+
+            features   : [
+                {
+                    ftype    : 'Ext.ux.touch.grid.feature.Paging',
+                    launchFn : 'initialize'
+                },
+                {
+                    ftype    : 'Ext.ux.touch.grid.feature.Sorter',
+                    launchFn : 'initialize'
+                }
+            ],
+            columns   : [
+                {
+                    header    : 'First Name',
+                    dataIndex : 'firstName',
+                    style     : 'padding-left: 1em;',
+                    width     : '30%',
+                    filter    : { type : 'string' }
+                },
+                {
+                    header    : 'Last Name',
+                    dataIndex : 'lastName',
+                    style     : 'padding-left: 1em;',
+                    width     : '30%',
+                    filter    : { type : 'numeric' }
+                },
+                {
+                    header    : 'Full Name',
+                    dataIndex : 'fullName',
+                    style     : 'padding-left: 1em;',
+                    width     : '40%'
+                }
+            ]
+        });
+    }
+});
