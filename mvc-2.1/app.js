edee0401109463936cd707a75435f2acab47205d
Pherris <aralston@peoplenetonline.com>
2013-03-21 10:47:46 -0500

    created new mvc example for sencha touch 2.1

---
+++ mvc-2.1/app.js
@@ -0,0 +1,63 @@
+//<debug>
+Ext.Loader.setPath({
+    'Ext':                  'touch/src',
+    'mvc-2.1':              'app',
+    'Ext.ux.touch.grid':    '../Ext.ux.touch.grid'
+});
+//</debug>
+
+Ext.application({
+    name: 'mvc-2.1',
+
+    requires: [
+        'Ext.MessageBox'
+    ],
+    
+    models: [
+         'TestModel'
+     ],
+     
+     stores: [
+         'TestStore'
+     ],
+
+    views: ['Main'],
+
+    icon: {
+        '57': 'resources/icons/Icon.png',
+        '72': 'resources/icons/Icon~ipad.png',
+        '114': 'resources/icons/Icon@2x.png',
+        '144': 'resources/icons/Icon~ipad@2x.png'
+    },
+
+    isIconPrecomposed: true,
+
+    startupImage: {
+        '320x460': 'resources/startup/320x460.jpg',
+        '640x920': 'resources/startup/640x920.png',
+        '768x1004': 'resources/startup/768x1004.png',
+        '748x1024': 'resources/startup/748x1024.png',
+        '1536x2008': 'resources/startup/1536x2008.png',
+        '1496x2048': 'resources/startup/1496x2048.png'
+    },
+
+    launch: function() {
+        // Destroy the #appLoadingIndicator element
+        Ext.fly('appLoadingIndicator').destroy();
+
+        // Initialize the main view
+        Ext.Viewport.add(Ext.create('mvc-2.1.view.Main'));
+    },
+
+    onUpdated: function() {
+        Ext.Msg.confirm(
+            "Application Update",
+            "This application has just successfully been updated to the latest version. Reload now?",
+            function(buttonId) {
+                if (buttonId === 'yes') {
+                    window.location.reload();
+                }
+            }
+        );
+    }
+});
---
+++ mvc-2.1/app/view/Main.js
@@ -0,0 +1,45 @@
+Ext.define('mvc-2.1.view.Main', {
+    extend: 'Ext.tab.Panel',
+    xtype: 'main',
+    requires: [
+        'Ext.TitleBar',
+        'mvc-2.1.view.Grid'
+    ],
+    config: {
+        tabBarPosition: 'bottom',
+        
+        items: [{
+                title: 'Grid Sample',
+                iconCls: 'action',
+                
+                layout:'fit',
+                
+                items : [{
+                    docked: 'top',
+                    xtype: 'titlebar',
+                    title: 'Ext.ux.touch.grid Example'
+                },{
+                    xtype : 'gridview'
+                }]
+            },{
+                title: 'Welcome',
+                iconCls: 'home',
+
+                styleHtmlContent: true,
+                scrollable: true,
+
+                items: {
+                    docked: 'top',
+                    xtype: 'titlebar',
+                    title: 'Welcome to Sencha Touch 2'
+                },
+
+                html: [
+                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
+                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
+                    "and refresh to change what's rendered here."
+                ].join("")
+            }
+        ]
+    }
+});
